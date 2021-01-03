907029f9a6269bb3075a4b5c677ed15594909623
Augusto Nunes <augusto.cesar@gmail.com>
2021-01-03 11:31:36 -0300

    Re-recomeçando

---
+++ content/posts/why-i-moved-to-hugo.md
@@ -0,0 +1,11 @@
+---
+title: "Why I Moved to Hugo"
+date: 2021-01-03T11:13:17-03:00
+draft: true
+---
+
+Well, I was very aggravated by Jekyll, Ruby and all it's dependency management, environment setup etc. 
+
+Let's try using Hugo this time. Maybe I can learn Go and finally kickstart this blogging project.
+
+Here goes nothing!
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Comando falha, deploy para
+set -e
+
+printf "\033[0;32m Deploy de atualizações para o Github... \033[0m\n"
+
+# Build no projeto
+hugo -t hugo-flex
+
+
+# Pasta public
+cd public
+
+# Adiciona modificações ao git
+git add .
+
+# Commit
+msg="Rebuild do site em $(date)"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+git commit -m "$msg"
+
+# Push no source e build
+git push origin main
